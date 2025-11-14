79bd7d4943d329ebf3d78e76e0d5e8ef2c37d6ed
parkgisu <su25578@naver.com>
2025-11-14 15:09:11 +0900

    feat: redesign ActionPacket to separate movement axes and attack flag

--- src/engine/dto/ActionPacket.java
+++ src/engine/dto/ActionPacket.java
@@ -6,6 +6,7 @@ package engine.dto;
  * /action Map the request body (JSON) of the endpoint
  */
 public class ActionPacket {
-    /** 0: stop, 1: left, 2: right, 3: up, 4: down, 5: atack */
-    public int action;
+    public int moveX; // -1, 0, +1
+    public int moveY; // -1, 0, +1
+    public boolean shoot;
 }
