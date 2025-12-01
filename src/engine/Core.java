30e0462db296e4d0b76712ab99b32356c0ddc69d
parkgisu <su25578@naver.com>
2025-12-02 01:38:22 +0900

    Fix: Add missing break after 1P mode score screen to prevent fall-through

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -192,6 +192,7 @@ public final class Core {
                     currentScreen = new ScoreScreen(width, height, FPS, gameState);
                     returnCode = frame.setScreen(currentScreen);
                     LOGGER.info("Closing score screen.");
+                    break;
 
                 case 3:
                     // High scores
