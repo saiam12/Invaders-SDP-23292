417cf56f0ad556312b21bf7c5b26d172ec28b40b
msssggg3-14 <ijaehu001@gmail.com>
2025-12-04 15:39:37 +0900

    git add useless annotation

--- src/engine/User.java
+++ src/engine/User.java
@@ -25,7 +25,7 @@ public class User {
 
 
     public Map<String, Boolean> getAchievements() {
-        return achievements; // 그냥 원본 맵을 반환해야 외부에서 put이 가능합니다.
+        return achievements;
     }
     public void setAchievementStatus(String name, boolean unlocked) {
         achievements.put(name, unlocked);
